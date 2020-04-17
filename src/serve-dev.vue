e7c5c2692160ff773550f2d09da9cb2ac1f95063
Javad Mnjd <javad.mnjd@hotmail.com>
2020-04-17 16:41:26 +0430

    test

--- src/serve-dev.vue
+++ src/serve-dev.vue
@@ -12,30 +12,30 @@ export default Vue.extend({
 
 <template>
   <div id="app">
-    <div v-for="i in 20">
-      <div
-        style="
-          margin: 1rem;
-          background-color: #eaeaea;
-          width: 40px;
-          height: 40px;
-        "
-      ></div>
-    </div>
+    <div
+      v-for="i in 20"
+      :key="i"
+      style="
+        margin: 1rem;
+        background-color: #eaeaea;
+        width: 40px;
+        height: 40px;
+      "
+    ></div>
     <vue-sticky-element>
       <div style="background-color: red; width: 100%; height: 50px;">
         test
       </div>
     </vue-sticky-element>
-    <div v-for="i in 30">
-      <div
-        style="
-          margin: 1rem;
-          background-color: #eaeaea;
-          width: 40px;
-          height: 40px;
-        "
-      ></div>
-    </div>
+    <div
+      v-for="i in 20"
+      :key="i"
+      style="
+        margin: 1rem;
+        background-color: #eaeaea;
+        width: 40px;
+        height: 40px;
+      "
+    ></div>
   </div>
 </template>
