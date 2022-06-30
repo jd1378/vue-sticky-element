4b6865f216a1846e18d4d1d84f5ef208a6966a65
Javad Mnjd <javad.mnjd@hotmail.com>
2022-06-30 20:29:13 +0430

    lint

--- src/serve-dev.vue
+++ src/serve-dev.vue
@@ -15,27 +15,15 @@ export default Vue.extend({
     <div
       v-for="i in 20"
       :key="i"
-      style="
-        margin: 1rem;
-        background-color: #eaeaea;
-        width: 40px;
-        height: 40px;
-      "
+      style="margin: 1rem; background-color: #eaeaea; width: 40px; height: 40px"
     ></div>
     <vue-sticky-element>
-      <div style="background-color: red; width: 100%; height: 50px;">
-        test
-      </div>
+      <div style="background-color: red; width: 100%; height: 50px">test</div>
     </vue-sticky-element>
     <div
       v-for="i in 20"
       :key="i"
-      style="
-        margin: 1rem;
-        background-color: #eaeaea;
-        width: 40px;
-        height: 40px;
-      "
+      style="margin: 1rem; background-color: #eaeaea; width: 40px; height: 40px"
     ></div>
   </div>
 </template>
